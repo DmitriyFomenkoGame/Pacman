c254da8edf66683558dc53a34edcf61b5348def9
ericdekruijf <ericdekruijf@gmail.com>
2013-02-21 22:46:16 +0100

    ManualController een GUI weer laten geven werkt

--- Pacman/src/Pacman/PacmanControllerManual.java
+++ Pacman/src/Pacman/PacmanControllerManual.java
@@ -1,9 +1,19 @@
 package Pacman;
 
+import GameUI.PacmanGUI;
+
 public class PacmanControllerManual {
 
 	public void start() {
-		
+		PacmanGUI gui = new PacmanGUI();
+		gui.show();
+		while(true) {
+			try {
+				Thread.sleep(5000);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 	}
 	
 	public static void main(String[] args) {
