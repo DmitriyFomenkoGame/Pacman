3871a9ee67f73117f2840009d8facc3d6e3fa963
ericdekruijf <ericdekruijf@gmail.com>
2013-02-21 21:36:32 +0100

    PacmanGame class aangevuld qua methoden die er nodig zijn en wat ze moeten kunnen

--- Pacman/src/Pacman/PacmanGame.java
+++ Pacman/src/Pacman/PacmanGame.java
@@ -3,24 +3,44 @@ package Pacman;
 public class PacmanGame {
 	
 	private Board board;
-	private int score, ghostseaten, gameticks;
+	private PacmanScore score;
+	private int gameStatus;
+	private int maxGameticks;
 	
 	public static final int DIR_UP    = 0,
 							DIR_RIGHT = 1,
 							DIR_DOWN  = 2,
 							DIR_LEFT  = 3;
 	
-	public PacmanGame() {
-		board 		= new Board();
-		score 		= 0;
-		ghostseaten = 0;
-		gameticks   = 0;
+	public static final int GAME_BUSY    = 0,
+							GAME_OVER    = 1,
+							GAME_END     = 2,
+							GAME_TIMEOUT = 3;
+	
+	public PacmanGame(int maxGameticks) { //Value <0 enables infinit gameticks
+		board 			  = new Board();
+		score 			  = new PacmanScore();
+		gameStatus		  = GAME_BUSY;
+		this.maxGameticks = maxGameticks;
 	}
 	
 	public void doMove(int direction) {
-		
+		if (gameStatus != GAME_BUSY) {return;}
+		score.addScore(board.doMove(direction));
+		if (maxGameticks > 0) {
+			if (score.getGameticks() > maxGameticks) {
+				gameStatus = GAME_TIMEOUT;
+			}
+		}
 	}
 	
+	public Board getBoard() {
+		return (Board) board.clone();
+	}
+	
+	public PacmanScore getScore() {
+		return (PacmanScore) score.clone();
+	}
 	
 	
 	
