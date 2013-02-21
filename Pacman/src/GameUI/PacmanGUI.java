63728477cfb6ce878ca797201031a2004f54522c
ericdekruijf <ericdekruijf@gmail.com>
2013-02-21 23:01:52 +0100

    Muren worden goed getekend, maar een werkend redraw mechanisme zou fijn zijn, alleen werken alle manieren niet tot het moment dat je resizet.

--- Pacman/src/GameUI/PacmanGUI.java
+++ Pacman/src/GameUI/PacmanGUI.java
@@ -1,14 +1,19 @@
 package GameUI;
 
 import java.awt.Color;
+import java.awt.image.BufferedImage;
 
 import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
 import Pacman.Board;
 
 public class PacmanGUI {
 	private JFrame frame;
 	private ImagePanel panel;
+	
+	public static final int BOARD_WIDTH  = Board.WIDTH,
+							BOARD_HEIGHT = Board.HEIGHT;
 	
 	public PacmanGUI() {
 		frame = new JFrame("Pacman GUI");
@@ -24,7 +29,17 @@ public class PacmanGUI {
 	}
 	
 	public void setBoard(Board b) {
-
+		BufferedImage image = new BufferedImage(BOARD_WIDTH, BOARD_HEIGHT, BufferedImage.TYPE_INT_RGB);
+		boolean[][] walls = b.getWalls();
+		for(int j = 0; j < BOARD_HEIGHT; j++) {
+			for(int i = 0; i < BOARD_WIDTH; i++) {
+				if (walls[i][j]) {
+					image.setRGB(i, j, Color.red.getRGB());
+				}
+			}
+		}
+		panel.setImage(image);
+		//TODO: Add redraw...
 	}
 	
 }
