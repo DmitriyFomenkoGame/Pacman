ef7fecea476f99f8ec8770e6263e130ca588cb12
ericdekruijf <ericdekruijf@gmail.com>
2013-02-21 22:38:25 +0100

    GUI tests toegevoegd aan Git

---
+++ Pacman/src/GameUI/GUItest.java
@@ -0,0 +1,49 @@
+package GameUI;
+
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.swing.JFrame;
+
+public class GUItest {
+
+	public void start() throws IOException, InterruptedException {
+		BufferedImage myImage = new BufferedImage(28, 31, BufferedImage.TYPE_INT_ARGB);
+		String field = "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWDDDDDDDDDDDDWWDDDDDDDDDDDDWWDWWWWDWWWWWDWWDWWWWWDWWWWDWWEWWWWDWWWWWDWWDWWWWWDWWWWEWWDWWWWDWWWWWDWWDWWWWWDWWWWDWWDDDDDDDDDDDDDDDDDDDDDDDDDDWWDWWWWDWWDWWWWWWWWDWWDWWWWDWWDWWWWDWWDWWWWWWWWDWWDWWWWDWWDDDDDDWWDDDDWWDDDDWWDDDDDDWWWWWWWDWWWWW WW WWWWWDWWWWWWWWWWWWDWWWWW WW WWWWWDWWWWWWWWWWWWDWW    G     WWDWWWWWWWWWWWWDWW WWWWWWWW WWDWWWWWWWWWWWWDWW W      W WWDWWWWWW      D   W G G GW   D      WWWWWWDWW W      W WWDWWWWWWWWWWWWDWW WWWWWWWW WWDWWWWWWWWWWWWDWW          WWDWWWWWWWWWWWWDWW WWWWWWWW WWDWWWWWWWWWWWWDWW WWWWWWWW WWDWWWWWWWDDDDDDDDDDDDWWDDDDDDDDDDDDWWDWWWWDWWWWWDWWDWWWWWDWWWWDWWDWWWWDWWWWWDWWDWWWWWDWWWWDWWEDDWWDDDDDDDPDDDDDDDDWWDDEWWWWDWWDWWDWWWWWWWWDWWDWWDWWWWWWDWWDWWDWWWWWWWWDWWDWWDWWWWDDDDDDWWDDDDWWDDDDWWDDDDDDWWDWWWWWWWWWWDWWDWWWWWWWWWWDWWDWWWWWWWWWWDWWDWWWWWWWWWWDWWDDDDDDDDDDDDDDDDDDDDDDDDDDWWWWWWWWWWWWWWWWWWWWWWWWWWWWW";
+		for(int j = 0; j < 31; j++) {
+			for(int i = 0; i < 28; i++) {
+				char c = field.charAt(j * 28 + i);
+				if (c == 'W') {
+					myImage.setRGB(i, j, Color.blue.getRGB());
+				} else if (c == ' ') {
+					myImage.setRGB(i, j, Color.black.getRGB());
+				} else if (c == 'D') {
+					myImage.setRGB(i, j, Color.gray.getRGB());
+				} else if (c == 'E') {
+					myImage.setRGB(i, j, Color.lightGray.getRGB());
+				} else if (c == 'P') {
+					myImage.setRGB(i, j, Color.yellow.getRGB());
+				} else {
+					myImage.setRGB(i, j, Color.red.getRGB());
+				}
+			}
+		}
+		JFrame myJFrame = new JFrame("Pacman test JONGUH");
+		ImagePanel imagePanel = new ImagePanel(myImage);
+		imagePanel.setSize(280, 310);
+		myJFrame.setContentPane(imagePanel);
+		myJFrame.setSize(280, 310);
+		myJFrame.setBackground(Color.black);
+		myJFrame.setVisible(true);
+		myJFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		while (true) {
+			Thread.sleep(5000);
+		}
+	}
+	
+	public static void main(String[] args) throws IOException, InterruptedException {
+		new GUItest().start();
+	}
+
+}
---
+++ Pacman/src/GameUI/ImagePanel.java
@@ -0,0 +1,46 @@
+package GameUI;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
+
+import javax.swing.JComponent;
+
+@SuppressWarnings("serial")
+public class ImagePanel extends JComponent implements ComponentListener {
+	private Image image;
+    
+    private Dimension size = null;
+    private int width   = 0,
+    	        height  = 0,
+    	        xoffset = 0,
+    	        yoffset = 0;
+    
+    public ImagePanel(Image image) {
+        this.image = image;
+        this.addComponentListener(this);
+    }
+    
+	public void componentHidden(ComponentEvent arg0) {}
+	public void componentMoved(ComponentEvent arg0) {}
+	public void componentShown(ComponentEvent arg0) {}
+	public void componentResized(ComponentEvent e) {
+    	size = this.getSize();
+    	float wfrac = size.width / 28, hfrac = size.height / 31;
+    	if (wfrac > hfrac) {
+    		height  = size.height;
+    		width   = (int) (28 * hfrac);
+    	} else {
+    		width   = size.width;
+    		height  = (int) (32 * wfrac);
+    	}
+		xoffset = (int) ((size.width - width)   * 0.5);
+		yoffset = (int) ((size.height - height) * 0.5);
+	}
+	
+    protected void paintComponent(Graphics g) {
+        g.drawImage(image, xoffset, yoffset, width, height, null);
+    }
+}
