e9c01625e154e189129c1843c701223e8dd96a36
ericdekruijf <ericdekruijf@gmail.com>
2013-02-21 21:17:14 +0100

    Pacman implementatie toegevoegd, hopelijk aan goede branch...

---
+++ Pacman/src/Pacman/PacmanControllerManual.java
@@ -0,0 +1,13 @@
+package Pacman;
+
+public class PacmanControllerManual {
+
+	public void start() {
+		System.out.println("Start");
+	}
+	
+	public static void main(String[] args) {
+		new PacmanControllerManual().start();
+	}
+
+}
---
+++ Pacman/src/Pacman/PacmanGame.java
@@ -0,0 +1,28 @@
+package Pacman;
+
+public class PacmanGame {
+	
+	private Board board;
+	private int score, ghostseaten, gameticks;
+	
+	public static final int DIR_UP    = 0,
+							DIR_RIGHT = 1,
+							DIR_DOWN  = 2,
+							DIR_LEFT  = 3;
+	
+	public PacmanGame() {
+		board 		= new Board();
+		score 		= 0;
+		ghostseaten = 0;
+		gameticks   = 0;
+	}
+	
+	public void doMove(int direction) {
+		
+	}
+	
+	
+	
+	
+	
+}
