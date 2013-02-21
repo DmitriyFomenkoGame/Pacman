2b38d2b8b2199e10c743637262753ea9cb0d2c56
ericdekruijf <ericdekruijf@gmail.com>
2013-02-21 21:29:37 +0100

    Score object toegevoegd om eenvoudig score te kunnen herleiden naar verschillende factoren. Op deze manier kunnen we later nog wijzigen hoe zwaar dingen meetellen.

---
+++ Pacman/src/Pacman/PacmanScore.java
@@ -0,0 +1,48 @@
+package Pacman;
+
+//Wrapper for the possible ways to get a higher score in pacman
+public class PacmanScore implements Cloneable {
+	private int dots, energizers, ghosts, gameticks;
+
+	public PacmanScore() {
+		dots       = 0;
+		energizers = 0;
+		ghosts     = 0;
+		gameticks  = 0;
+	}
+
+	public void addDot() {
+		dots++;
+	}
+	public void addEnergizer() {
+		energizers++;
+	}
+	public void addGhost() {
+		ghosts++;
+	}
+	public void addGametick() {
+		gameticks++;
+	}
+
+	public void addScore(PacmanScore p) {
+		this.dots 		+= p.dots;
+		this.energizers += p.energizers;
+		this.ghosts 	+= p.ghosts;
+		this.gameticks 	+= p.gameticks;
+	}
+
+	public Object clone(){
+		try{
+			PacmanScore cloned = (PacmanScore) super.clone();
+			cloned.dots       = dots;
+			cloned.energizers = energizers;
+			cloned.ghosts     = ghosts;
+			cloned.gameticks  = gameticks;
+			return cloned;
+		}
+		catch(CloneNotSupportedException e){
+			System.out.println(e);
+			return null;
+		}
+	}
+}
