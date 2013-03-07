621acee37053d0f42f6ab479e433415bbce7884b
Luka Stout <lukastout@gmail.com>
2013-03-07 12:58:43 +0100

    ActivatorData aangemaakt. Ga hierin verder werken. DO NOT TOUCH PLZ. KTHXBAI

---
+++ Pacman/src/Anji/ActivatorData.java
@@ -0,0 +1,14 @@
+package Anji;
+
+import com.anji.util.Configurable;
+import com.anji.util.Properties;
+
+public class ActivatorData implements Configurable {
+
+	@Override
+	public void init(Properties arg0) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
