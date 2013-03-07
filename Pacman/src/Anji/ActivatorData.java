9f0012ff7799795e01499b5f37f3161b5ab6b0a1
ericdekruijf <ericdekruijf@gmail.com>
2013-03-07 18:21:23 +0100

    Nog wat properties erbij gezet, voor directions van pacman/ghosts

--- Pacman/src/Anji/ActivatorData.java
+++ Pacman/src/Anji/ActivatorData.java
@@ -16,12 +16,14 @@ public class ActivatorData {
 		 * - 'Gameticks remaining'
 		 * - Score
 		 * - Scatter vs chase vs fright mode
+		 * - Pacmandirection
+		 * - Ghostdirection
 		 * - ...
 		 * 
 		 * TODO Uitzoeken of het uitmaakt of je een grote 1D array meegeeft of een 2D array
 		 * Het formatten van de data:
 		 * - Binaire grids vs 'één grid'
-		 * - Volledige bord vs zichtveld (vb zichtveld == -1 --> volledige bord)
+		 * - Volledige bord vs zichtveld (vb zichtveld == -1 --> volledige bord en zichtveld > 0 --> aantal pixels naar links/rechts/boven/onder, dus 1 is een 3*3 rondom pacman)
 		 * - ...
 		 * 
 		 */
