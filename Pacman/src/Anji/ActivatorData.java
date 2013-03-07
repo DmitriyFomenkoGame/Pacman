591ba12b3c6ef501da41e9bc411afbbd1ff86afe
ericdekruijf <ericdekruijf@gmail.com>
2013-03-07 18:06:58 +0100

    Configurable eruit gegooid voor de ActivatorData, zonder is makkelijker, care om netheid.

--- Pacman/src/Anji/ActivatorData.java
+++ Pacman/src/Anji/ActivatorData.java
@@ -1,18 +1,11 @@
 package Anji;
 
-import com.anji.util.Configurable;
 import com.anji.util.Properties;
 
-public class ActivatorData implements Configurable {
+public class ActivatorData {
 
-	@Override
-	public void init(Properties properties) throws Exception {
-		try{
-			
-		}catch(Exception e){
-			throw new IllegalArgumentException( "invalid properties: " + e.getClass().toString() + ": "
-					+ e.getMessage() );
-		}
+	public void init(Properties properties) {
+
 	}
 
 }
