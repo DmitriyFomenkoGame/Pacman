8737db6fef4c73d70bfcba8e7be5e6d8a3f68498
ericdekruijf <ericdekruijf@gmail.com>
2013-03-08 00:55:33 +0100

    ActivatorData properties toegevoegd

--- Pacman/src/Anji/ActivatorData.java
+++ Pacman/src/Anji/ActivatorData.java
@@ -4,29 +4,34 @@ import com.anji.util.Properties;
 
 public class ActivatorData {
 
+	private boolean showPacman, showBlinky, showPinky, showInky, showClyde, showWalls, showDots, showEnergizers;
+	private boolean showGameticks, showScore, showMode;
+	private boolean showPacmandir, showGhostsdir;
+	
+	private boolean isSingleGrid;
+	private int viewSize;
+	
 	public void init(Properties properties) {
+		showPacman = properties.getBooleanProperty("activator.data.pacman.show", true);
 		
-		/* Mogelijke properties:
-		 * 
-		 * Booleans voor dingen wel/niet meegeven aan het neurale netwerk:
-		 * - Pacman
-		 * - Blinky / Pinky / Inky / Clyde
-		 * - Walls
-		 * - Dots
-		 * - Energizers
-		 * - 'Gameticks remaining'
-		 * - Score
-		 * - Scatter vs chase vs fright mode
-		 * - Pacmandirection
-		 * - Ghostdirection
-		 * - ...
-		 * 
-		 * Het formatten van de data:
-		 * - Binaire grids vs 'één grid'
-		 * - Volledige bord vs zichtveld (vb zichtveld == -1 --> volledige bord en zichtveld > 0 --> aantal pixels naar links/rechts/boven/onder, dus 1 is een 3*3 rondom pacman)
-		 * - ...
-		 * 
-		 */
+		showBlinky = properties.getBooleanProperty("activator.data.blinky.show", true);
+		showPinky  = properties.getBooleanProperty("activator.data.pinky.show",  true);
+		showInky   = properties.getBooleanProperty("activator.data.inky.show",   true);
+		showClyde  = properties.getBooleanProperty("activator.data.clyde.show",  true);
+		
+		showWalls  	   = properties.getBooleanProperty("activator.data.walls.show", 	 true);
+		showDots  	   = properties.getBooleanProperty("activator.data.dots.show", 	     true);
+		showEnergizers = properties.getBooleanProperty("activator.data.energizers.show", true);
+ 		
+		showGameticks = properties.getBooleanProperty("activator.data.gameticks.show", false);
+		showScore     = properties.getBooleanProperty("activator.data.score.show",     false);
+		showMode      = properties.getBooleanProperty("activator.data.mode.show",      false);
+		
+		showPacmandir = properties.getBooleanProperty("activator.data.pacman.dir.show", false);
+		showGhostsdir = properties.getBooleanProperty("activator.data.ghosts.dir.show", false);
+		
+		isSingleGrid  = properties.getBooleanProperty("activator.data.representation.singlegrid", true);
+		viewSize      = properties.getIntProperty("activator.data.representation.viewsize", -1);
 	}
 
 }
