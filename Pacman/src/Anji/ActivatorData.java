e9d84b1b6cbe0fe5503b9fee2b7672b4661a9562
ericdekruijf <ericdekruijf@gmail.com>
2013-03-07 18:11:45 +0100

    Mogelijkheden voor properties gelist in ActivatorData

--- Pacman/src/Anji/ActivatorData.java
+++ Pacman/src/Anji/ActivatorData.java
@@ -5,7 +5,26 @@ import com.anji.util.Properties;
 public class ActivatorData {
 
 	public void init(Properties properties) {
-
+		/* Mogelijke properties:
+		 * 
+		 * Booleans voor dingen wel/niet meegeven aan het neurale netwerk:
+		 * - Pacman
+		 * - Blinky / Pinky / Inky / Clyde
+		 * - Walls
+		 * - Dots
+		 * - Energizers
+		 * - 'Gameticks remaining'
+		 * - Score
+		 * - Scatter vs chase vs fright mode
+		 * - ...
+		 * 
+		 * TODO Uitzoeken of het uitmaakt of je een grote 1D array meegeeft of een 2D array
+		 * Het formatten van de data:
+		 * - Binaire grids vs 'één grid'
+		 * - Volledige bord vs zichtveld (vb zichtveld == -1 --> volledige bord)
+		 * - ...
+		 * 
+		 */
 	}
 
 }
